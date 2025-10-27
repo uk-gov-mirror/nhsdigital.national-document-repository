7287cdfa86ef57bc9b477b17194b400cb024653d
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2025-10-27 15:14:11 +0000

    [PRMP-401] Parse scan date in bulk upload (#795)

--- lambdas/tests/unit/utils/test_lloyd_george_validator.py
+++ lambdas/tests/unit/utils/test_lloyd_george_validator.py
@@ -1,9 +1,10 @@
 import pytest
 from botocore.exceptions import ClientError
+from requests import Response
+
 from enums.supported_document_types import SupportedDocumentTypes
 from enums.validation_score import ValidationResult, ValidationScore
 from models.pds_models import Patient
-from requests import Response
 from services.base.ssm_service import SSMService
 from services.document_service import DocumentService
 from tests.unit.conftest import TEST_NHS_NUMBER, expect_not_to_raise
@@ -57,7 +58,7 @@ from utils.lloyd_george_validator import (
     validate_patient_date_of_birth,
     validate_patient_name_lenient,
     validate_patient_name_strict,
-    validate_patient_name_using_full_name_history,
+    validate_patient_name_using_full_name_history, validate_scan_date,
 )
 
 
@@ -1346,3 +1347,14 @@ def test_calculate_validation_score_for_lenient_check(
     )
     assert historical == expected_historical_match
     assert actual_result == expected_score
+
+
+def test_validates_correct_scan_date_format():
+    scan_date = "12-03-2023"
+    result = validate_scan_date(scan_date)
+    assert result == "2023-03-12"
+
+@pytest.mark.parametrize("scan_date", ["", "not-a-date", "1678246", "12/12/2024 12:12", "2023/03/12"])
+def test_raises_exception_for_invalid_scan_date_format(scan_date):
+    with pytest.raises(LGInvalidFilesException, match="Invalid scan date format"):
+        validate_scan_date(scan_date)
