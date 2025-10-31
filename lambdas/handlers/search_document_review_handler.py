1b5be45a956f6b977ba6f7947b73a54328f3e3bf
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-10-31 15:21:35 +0000

    [PRMP-589] format

--- lambdas/handlers/search_document_review_handler.py
+++ lambdas/handlers/search_document_review_handler.py
@@ -1,6 +1,26 @@
 from services.search_document_review_service import SearchDocumentReviewService
+from utils.decorators.ensure_env_var import ensure_environment_variables
+from utils.decorators.handle_lambda_exceptions import handle_lambda_exceptions
+from utils.decorators.set_audit_arg import set_request_context_for_logging
 from utils.exceptions import OdsErrorException
 from utils.request_context import request_context
+
+
+
+@ensure_environment_variables(
+    names=[
+        "DOCUMENT_REVIEW_DYNAMODB_NAME"
+    ]
+)
+@handle_lambda_exceptions
+@set_request_context_for_logging
+def lambda_handler(event, context):
+    osd_code = get_ods_code_from_request()
+
+    service = SearchDocumentReviewService(osd_code)
+
+    service.get_review_document_references()
+
 
 
 def get_ods_code_from_request():
@@ -11,9 +31,3 @@ def get_ods_code_from_request():
         raise OdsErrorException("No ODS code provided")
 
     return ods_code
-
-def lambda_handler(event, context):
-    osd_code = get_ods_code_from_request()
-    service = SearchDocumentReviewService(osd_code)
-    service.get_review_document_references()
-    pass
