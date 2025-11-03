0d0b3c4d69171fce6178afd374d5da2581fa8a32
SWhyteAnswer <sam.whyte@answerdigital.com>
2025-11-03 11:16:39 +0000

    [PRMP-570] Exceptions added for 198

--- lambdas/scripts/dynamodb_migration_prmp_179.py
+++ lambdas/scripts/dynamodb_migration_prmp_179.py
@@ -1,7 +1,6 @@
 from typing import Callable, Iterable
 
-from lambdas.utils.exceptions import MigrationUnrecoverableException
-from lambdas.utils.exceptions import MigrationRetryableException
+from lambdas.utils.exceptions import MigrationUnrecoverableException, MigrationRetryableException
 from scripts.MigrationBase import MigrationBase
 from services.base.dynamo_service import DynamoDBService
 from services.base.s3_service import S3Service
