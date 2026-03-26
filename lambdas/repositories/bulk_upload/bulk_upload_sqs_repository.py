f8c3dc4e53b911012864df8e083d6ea432638d77
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-03-26 11:07:53 +0000

    [PRMP-1626] Metadata Processor preprocessor failure is causing all subsequent logs of Patient NHS Number for remaining patients (#1210)
    Co-authored-by: Robert Gaskin <106234256+robg-test@users.noreply.github.com>

--- lambdas/repositories/bulk_upload/bulk_upload_sqs_repository.py
+++ lambdas/repositories/bulk_upload/bulk_upload_sqs_repository.py
@@ -19,15 +19,18 @@ class BulkUploadSqsRepository:
         self.review_queue_url = os.getenv("REVIEW_SQS_QUEUE_URL")
 
     def put_staging_metadata_back_to_queue(self, staging_metadata: StagingSqsMetadata):
-        request_context.patient_nhs_no = staging_metadata.nhs_number
-        setattr(staging_metadata, "retries", (staging_metadata.retries + 1))
-        logger.info("Returning message to sqs queue...")
-        self.sqs_repository.send_message_with_nhs_number_attr_fifo(
-            queue_url=self.metadata_queue_url,
-            message_body=staging_metadata.model_dump_json(by_alias=True),
-            nhs_number=staging_metadata.nhs_number,
-            group_id=f"back_to_queue_bulk_upload_{uuid.uuid4()}",
-        )
+        try:
+            request_context.patient_nhs_no = staging_metadata.nhs_number
+            setattr(staging_metadata, "retries", (staging_metadata.retries + 1))
+            logger.info("Returning message to sqs queue...")
+            self.sqs_repository.send_message_with_nhs_number_attr_fifo(
+                queue_url=self.metadata_queue_url,
+                message_body=staging_metadata.model_dump_json(by_alias=True),
+                nhs_number=staging_metadata.nhs_number,
+                group_id=f"back_to_queue_bulk_upload_{uuid.uuid4()}",
+            )
+        finally:
+            request_context.patient_nhs_no = None
 
     def send_message_to_review_queue(
         self,
@@ -35,49 +38,57 @@ class BulkUploadSqsRepository:
         uploader_ods: str,
         failure_reason: DocumentReviewReason,
     ):
-        request_context.patient_nhs_no = staging_metadata.nhs_number
-        review_files = [
-            ReviewMessageFile(
-                file_name=file.file_path.split("/")[-1],
-                file_path=file.file_path.lstrip("/"),
+        try:
+            request_context.patient_nhs_no = staging_metadata.nhs_number
+            review_files = [
+                ReviewMessageFile(
+                    file_name=file.file_path.split("/")[-1],
+                    file_path=file.file_path.lstrip("/"),
+                )
+                for file in staging_metadata.files
+            ]
+
+            upload_id = f"{uuid.uuid4()}"
+
+            review_message = ReviewMessageBody(
+                upload_id=upload_id,
+                files=review_files,
+                nhs_number=staging_metadata.nhs_number,
+                failure_reason=failure_reason,
+                uploader_ods=uploader_ods,
             )
-            for file in staging_metadata.files
-        ]
 
-        upload_id = f"{uuid.uuid4()}"
+            logger.info(
+                f"Sending message to review queue for NHS number {staging_metadata.nhs_number} "
+                f"with failure reason: {failure_reason}",
+            )
 
-        review_message = ReviewMessageBody(
-            upload_id=upload_id,
-            files=review_files,
-            nhs_number=staging_metadata.nhs_number,
-            failure_reason=failure_reason,
-            uploader_ods=uploader_ods,
-        )
-
-        logger.info(
-            f"Sending message to review queue for NHS number {staging_metadata.nhs_number} "
-            f"with failure reason: {failure_reason}",
-        )
-
-        self.sqs_repository.send_message_standard(
-            queue_url=self.review_queue_url,
-            message_body=review_message.model_dump_json(),
-        )
+            self.sqs_repository.send_message_standard(
+                queue_url=self.review_queue_url,
+                message_body=review_message.model_dump_json(),
+            )
+        finally:
+            request_context.patient_nhs_no = None
 
     def put_sqs_message_back_to_queue(self, sqs_message: dict):
-        try:
-            nhs_number = sqs_message["messageAttributes"]["NhsNumber"]["stringValue"]
-            request_context.patient_nhs_no = nhs_number
-        except KeyError:
-            nhs_number = ""
-
-        logger.info("Returning message to sqs queue...")
-        self.sqs_repository.send_message_with_nhs_number_attr_fifo(
-            queue_url=self.metadata_queue_url,
-            message_body=sqs_message["body"],
-            nhs_number=nhs_number,
-            group_id=f"back_to_queue_bulk_upload_{uuid.uuid4()}",
+        nhs_number = (
+            sqs_message.get("messageAttributes", {})
+            .get("NhsNumber", {})
+            .get("stringValue", "")
         )
+
+        try:
+            request_context.patient_nhs_no = nhs_number or None
+
+            logger.info("Returning message to sqs queue...")
+            self.sqs_repository.send_message_with_nhs_number_attr_fifo(
+                queue_url=self.metadata_queue_url,
+                message_body=sqs_message["body"],
+                nhs_number=nhs_number,
+                group_id=f"back_to_queue_bulk_upload_{uuid.uuid4()}",
+            )
+        finally:
+            request_context.patient_nhs_no = None
 
     def send_message_to_pdf_stitching_queue(
         self,
